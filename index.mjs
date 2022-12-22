b521cfa062fa769aec5f5d2c078c852852b7b2a2
冯不游 <71683364+mefengl@users.noreply.github.com>
2022-12-22 23:25:32 +0800

    add: can use same conversation

--- index.mjs
+++ index.mjs
@@ -1,18 +1,32 @@
 import { ChatGPTAPIBrowser } from 'chatgpt'
 import express from 'express'
 
-// use puppeteer to bypass cloudflare (headful because of captchas)
-const api = new ChatGPTAPIBrowser({
-  email: process.env.OPENAI_EMAIL,
-  password: process.env.OPENAI_PASSWORD
-})
-
+const api = new ChatGPTAPIBrowser({ email: process.env.OPENAI_EMAIL, password: process.env.OPENAI_PASSWORD })
 await api.initSession()
 
 async function say(what) {
   try {
-    const result = await api.sendMessage(what)
-    return result?.response
+    const { response, conversationId, messageId } = await api.sendMessage(what) || {}
+    return {
+      answer: response,
+      sameConversationUrlComponent: `&conversationId=${conversationId}&parentMessageId=${messageId}`
+    }
+  } catch (e) {
+    console.log(e)
+    return 'Error'
+  }
+}
+
+async function old_say({ what, conversationId, parentMessageId }) {
+  try {
+    const { response, messageId } = await api.sendMessage(what, {
+      conversationId,
+      parentMessageId
+    }) || {}
+    return {
+      answer: response,
+      sameConversationUrlComponent: `&conversationId=${conversationId}&parentMessageId=${messageId}`
+    }
   } catch (e) {
     console.log(e)
     return 'Error'
@@ -22,10 +36,20 @@ async function say(what) {
 const app = express()
 
 app.get('/', async (req, res) => {
+  const { q: what, conversationId, parentMessageId } = req.query
+
+  if (conversationId && parentMessageId) {
+    const result = await old_say({ what, conversationId, parentMessageId })
+    res.send(result)
+    return
+  }
+
   const result = await say(req.query.q)
   res.send(result)
 })
 
 app.listen(8661, () => {
   console.log('Listening on port 8661')
+  console.log('New Conversation Example:\n http://localhost:8661/?q=Hello')
+  console.log('Old Conversation Example:\n http://localhost:8661/?q=Hello&conversationId=123&parentMessageId=456')
 })
